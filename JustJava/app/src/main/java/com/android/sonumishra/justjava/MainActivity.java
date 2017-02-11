96c903724b4fe00ab465a1103ebf33263777c07d
mishrasonu1993 <mishrasonu1993@gmail.com>
2017-02-11 15:49:38 -0800

    JustJava: made just-java application little more interactive

--- JustJava/app/src/main/java/com/android/sonumishra/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/android/sonumishra/justjava/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
@@ -58,14 +58,5 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
