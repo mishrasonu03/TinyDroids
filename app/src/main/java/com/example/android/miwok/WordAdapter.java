fd16363ac71384b0a62ea6bb09446fbaafda2e93
mishrasonu1993 <mishrasonu1993@gmail.com>
2017-02-13 00:25:58 -0800

    Miwok: added word-lists and corresponding translations

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,38 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class WordAdapter extends ArrayAdapter<Word>{
+
+    public WordAdapter(Context context, List<Word> objects){
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
